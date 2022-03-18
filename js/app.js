7acb2c0f2a9e6bd02f9d2c9fa4dad7b154954e7f
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-03-18 15:50:32 -0700

    initialize function for all play area clicks

--- js/app.js
+++ js/app.js
@@ -1,73 +1,8 @@
-// /*----- constants -----*/
-// const suits = ['s', 'c', 'd', 'h'];
-// const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+/*-----GLOBALLY SCOPED VARIABLES-----*/
 
-// // Build a 'master' deck of 'card' objects used to create shuffled decks
-// const masterDeck = buildMasterDeck();
-// renderDeckInContainer(masterDeck, document.getElementById('master-deck-container'));
-
-// /*----- app's state (variables) -----*/
-// let shuffledDeck;
-
-// /*----- cached element references -----*/
-// const shuffledContainer = document.getElementById('shuffled-deck-container');
-
-// /*----- event listeners -----*/
-// document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
-
-// /*----- functions -----*/
-// function getNewShuffledDeck() {
-//   // Create a copy of the masterDeck (leave masterDeck untouched!)
-//   const tempDeck = [...masterDeck];
-//   const newShuffledDeck = [];
-//   while (tempDeck.length) {
-//     // Get a random index for a card still in the tempDeck
-//     const rndIdx = Math.floor(Math.random() * tempDeck.length);
-//     // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-//     newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-//   }
-//   return newShuffledDeck;
-// }
-
-// function renderNewShuffledDeck() {
-//   // Create a copy of the masterDeck (leave masterDeck untouched!)
-//   shuffledDeck = getNewShuffledDeck();
-//   renderDeckInContainer(shuffledDeck, shuffledContainer);
-// }
-
-// function renderDeckInContainer(deck, container) {
-//   container.innerHTML = '';
-//   // Let's build the cards as a string of HTML
-//   let cardsHtml = '';
-//   deck.forEach(function(card) {
-//     cardsHtml += `<div class="card ${card.face}"></div>`;
-//   });
-//   // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
-//   // const cardsHtml = deck.reduce(function(html, card) {
-//   //   return html + `<div class="card ${card.face}"></div>`;
-//   // }, '');
-//   container.innerHTML = cardsHtml;
-// }
-
-// function buildMasterDeck() {
-//   const deck = [];
-//   // Use nested forEach to generate card objects
-//   suits.forEach(function(suit) {
-//     ranks.forEach(function(rank) {
-//       deck.push({
-//         // The 'face' property maps to the library's CSS classes for cards
-//         face: `${suit}${rank}`,
-//         // Setting the 'value' property for game of blackjack, not war
-//         value: Number(rank) || (rank === 'A' ? 11 : 10)
-//       });
-//     });
-//   });
-//   return deck;
-// }
-
-// renderNewShuffledDeck();
-
-// DECLARE GLOBALLY SCOPED VARIABLES
+// Card Val Arrays
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
 
 // Buttons
 const turnOneBtn = document.getElementById('turnOne');
@@ -77,25 +12,10 @@ const newGameBtn = document.getElementById('newGame');
 const timerBtn = document.getElementById('timerBtn');
 
 // Play Area
-
 const playAreaEl = document.getElementById('playArea');
-
-// const deckEl = document.getElementById('deck');
-// const discardEl = document.getElementById('discard');
-// const aceZoneOneEl = document.getElementById('aceZoneOne');
-// const aceZoneTwoEl = document.getElementById('aceZoneTwo');
-// const aceZoneThreeEl = document.getElementById('aceZoneThree');
-// const aceZoneFourEl = document.getElementById('aceZoneFour');
-// const playStackOneEl = document.getElementById('playStackOne');
-// const playStackTwoEl = document.getElementById('playStackTwo');
-// const playStackThreeEl = document.getElementById('playStackThree');
-// const playStackFourEl = document.getElementById('playStackFour');
-// const playStackFiveEl = document.getElementById('playStackFive');
-// const playStackSixEl = document.getElementById('playStackSix');
-// const playStackSevenEl = document.getElementById('playStackSeven');
+const masterDeckEl = document.getElementById('deck');
 
 // Stats
-
 const timerHrsEl = document.getElementById('timerHrs');
 const timerMinsEl = document.getElementById('timerMins');
 const timerSecsEl = document.getElementById('timerSecs');
@@ -103,12 +23,11 @@ const movesNumEl = document.getElementById('movesNum');
 const stockNumEl = document.getElementById('stockNum');
 
 // Timer Vars
-
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
 
-// DECLARE GLOBALLY SCOPED EVENT LISTENERS
+/*-----DECLARE GLOBALLY SCOPED EVENT LISTENERS-----*/
 
 // Button click listeners
 const turnOneClick = turnOneBtn.addEventListener('click', turnOne);
@@ -120,7 +39,72 @@ const timerClick = timerBtn.addEventListener('click', timerStartStop);
 // Play area click listeners
 const playAreaClick = playAreaEl.addEventListener('click', cardClick);
 
-// --- FUNCTIONS ---
+// Build a 'master' deck of 'card' objects used to create shuffled decks
+const masterDeck = buildMasterDeck();
+renderDeckInContainer(masterDeck, masterDeckEl);
+
+/*----- app's state (variables) -----*/
+let shuffledDeck;
+
+/*----- cached element references -----*/
+const shuffledContainer = document.getElementById('deck');
+
+/*----- event listeners -----*/
+document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
+
+/*----- functions -----*/
+function getNewShuffledDeck() {
+  // Create a copy of the masterDeck (leave masterDeck untouched!)
+  const tempDeck = [...masterDeck];
+  const newShuffledDeck = [];
+  while (tempDeck.length) {
+    // Get a random index for a card still in the tempDeck
+    const rndIdx = Math.floor(Math.random() * tempDeck.length);
+    // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+  }
+  return newShuffledDeck;
+}
+
+function renderNewShuffledDeck() {
+  // Create a copy of the masterDeck (leave masterDeck untouched!)
+  shuffledDeck = getNewShuffledDeck();
+  renderDeckInContainer(shuffledDeck, shuffledContainer);
+}
+
+function renderDeckInContainer(deck, container) {
+  container.innerHTML = '';
+  // Let's build the cards as a string of HTML
+  let cardsHtml = '';
+  deck.forEach(function(card) {
+    cardsHtml += `<div class="card ${card.face}"></div>`;
+  });
+  // Or, use reduce to 'reduce' the array into a single thing - in this case a string of HTML markup 
+  // const cardsHtml = deck.reduce(function(html, card) {
+  //   return html + `<div class="card ${card.face}"></div>`;
+  // }, '');
+  container.innerHTML = cardsHtml;
+}
+
+function buildMasterDeck() {
+  const deck = [];
+  // Use nested forEach to generate card objects
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push({
+        // The 'face' property maps to the library's CSS classes for cards
+        face: `${suit}${rank}`,
+        // Setting the 'value' property for game of blackjack, not war
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      });
+    });
+  });
+  return deck;
+}
+
+renderNewShuffledDeck();
+
+/*-----FUNCTIONS-----*/
 
 // INIT
 // Init() function starts a new game.
@@ -128,6 +112,7 @@ const playAreaClick = playAreaEl.addEventListener('click', cardClick);
 // Deal out cards to the play stacks.
 
 function init() {
+  renderNewShuffledDeck();
   console.log('The NEW GAME button was clicked');
 }
 
@@ -143,25 +128,21 @@ function turnThree() {
 
 function undo() {
   console.log('The UNDO button was clicked');
-}
+} 
 
 function timerStartStop() {
   if (timerBtn.dataset.status === 'inactive') {
-    timerBtn.dataset.status = 'active';
-    timerBtn.classList.remove('start');
-    timerBtn.classList.add('stop');
-    timerBtn.innerText = 'Stop';
     timerStart();
   } else if (timerBtn.dataset.status === 'active') {
-    timerBtn.dataset.status = 'inactive';
-    timerBtn.classList.remove('stop');
-    timerBtn.classList.add('start');
-    timerBtn.innerText = 'Start';
-    clearTimeout(timeElapsed);
+    timerStop();
   }
 }
 
 function timerStart() {
+  timerBtn.dataset.status = 'active';
+  timerBtn.classList.remove('start');
+  timerBtn.classList.add('stop');
+  timerBtn.innerText = 'Stop';
   timeElapsed = setTimeout(function() {
     seconds++;
     if (seconds > 59) {
@@ -191,10 +172,48 @@ function timerStart() {
   }, 1000);
 }
 
+function timerStop() {
+  timerBtn.dataset.status = 'inactive';
+  timerBtn.classList.remove('stop');
+  timerBtn.classList.add('start');
+  timerBtn.innerText = 'Start';
+  clearTimeout(timeElapsed);
+}
+
 // Card Click Function
 
 function cardClick(e) {
   if (e.target.id !== 'playArea') {
     console.log(`${e.target.id}`)
+    switch (true) {
+      case (e.target.id === 'deck'):
+        deckClick();
+        break;
+      case (e.target.id === 'discard'):
+        discardClick();
+        break;
+      case (e.target.dataset.location === 'aceZones'):
+        aceZoneClick();
+        break;
+      case (e.target.dataset.location === 'playStacks'):
+        playStacksClick();
+        break;
+    }
   } else {return;}
+}
+
+function deckClick() {
+  console.log('The deck was clicked');
+}
+
+function discardClick() {
+  console.log('The discard was clicked');
+}
+
+function aceZoneClick() {
+  console.log('One of the ace zones was clicked');
+}
+
+function playStacksClick() {
+  console.log('One of the play stacks was clicked');
 }
